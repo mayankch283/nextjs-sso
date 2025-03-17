2343fb96fcbdf6ac7f97b6d16ddfe9d62d9faff0
mayankch283 <mayankch283@gmail.com>
2025-03-18 01:31:45 +0530

    homepage for sso idp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
-import Image from "next/image";
+import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        Boilerplate
-
-        
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        WIP
-      </footer>
+    <div className="container">
+      <h1>Identity Provider Home</h1>
+      <p>Welcome to our Identity Provider service.</p>
+      <Link href="/login">
+        Login
+      </Link>
     </div>
   );
 }
