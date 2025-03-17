b11251bc61b6233f22b462f38259c22fd695e034
mayankch283 <mayankch283@gmail.com>
2025-03-17 19:06:50 +0530

    create userModel route

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,18 +2,25 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: {
+    username: {
       type: String,
-      required: [true, 'Please enter your first name'],
-      min: 3,
+      required: [true, 'Please enter your username'],
       max: 255,
+      min: 5,
+      unique: true,
     },
-    lastName: {
-      type: String,
-      required: [true, 'Please enter your last name'],
-      min: 3,
-      max: 255,
-    },
+    // firstName: {
+    //   type: String,
+    //   required: [true, 'Please enter your first name'],
+    //   min: 3,
+    //   max: 255,
+    // },
+    // lastName: {
+    //   type: String,
+    //   required: [true, 'Please enter your last name'],
+    //   min: 3,
+    //   max: 255,
+    // },
     email: {
       type: String,
       max: 255,
@@ -22,55 +29,49 @@ const userSchema = new mongoose.Schema(
       unique: true,
       index: true,
       default: null,
-    },
-    phoneNumber: {
-      type: String,
-      sparse: true,
-      unique: true,
-      index: true,
-      default: null,
-    },
+    }
+    ,
+    // phoneNumber: {
+    //   type: String,
+    //   sparse: true,
+    //   unique: true,
+    //   index: true,
+    //   default: null,
+    // },
     password: {
       type: String,
       required: [true, 'Please enter your password'],
       max: 1024,
       min: 5,
     },
-    username: {
-      type: String,
-      required: [true, 'Please enter your username'],
-      max: 255,
-      min: 5,
-      unique: true,
-    },
-    isEmailVerified: {
-      type: Boolean,
-      default: false,
-    },
-    isPhoneVerified: {
-      type: Boolean,
-      default: false,
-    },
-    isActive: {
-      type: Boolean,
-      default: false,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-    mfa_enabled: {
-      type: Boolean,
-      default: false,
-    },
-    forgotPasswordToken: String,
-    forgotPasswordTokenExpiry: Date,
-    emailVerificationOTP: String,
-    phoneVerificationOTP: String,
-    verificationOTPExpiry: Date,
-    mfa_otp: String,
-    mfa_expiry: Date,
-    lastLogin: Date,
+    // isEmailVerified: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    // isPhoneVerified: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    // isActive: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    // isAdmin: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    // mfa_enabled: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    // forgotPasswordToken: String,
+    // forgotPasswordTokenExpiry: Date,
+    // emailVerificationOTP: String,
+    // phoneVerificationOTP: String,
+    // verificationOTPExpiry: Date,
+    // mfa_otp: String,
+    // mfa_expiry: Date,
+    // lastLogin: Date,
   },
   {
     timestamps: true,
@@ -78,18 +79,19 @@ const userSchema = new mongoose.Schema(
 );
 
 // Add compound index to ensure either email or phone is present
-userSchema.index(
-  { email: 1, phoneNumber: 1 },
-  {
-    partialFilterExpression: {
-      $or: [
-        { email: { $type: 'string' } },
-        { phoneNumber: { $type: 'string' } },
-      ],
-    },
-  },
-);
+// userSchema.index(
+//   { email: 1, phoneNumber: 1 },
+//   {
+//     partialFilterExpression: {
+//       $or: [
+//         { email: { $type: 'string' } },
+//         { phoneNumber: { $type: 'string' } },
+//       ],
+//     },
+//   },
+// );
 
 const User = mongoose.models.users || mongoose.model('users', userSchema);
 
 export default User;
+
